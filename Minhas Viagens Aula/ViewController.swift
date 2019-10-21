c4d3ac40214fd1e82b41dd205b9e2afa08a62412
Matheus Rodrigues Araujo <matheusaraujo@NBBV016619.BRQ.COM>
2019-10-21 11:25:31 -0300

    Iniciando projeto e criando inteface inicial

---
+++ Minhas Viagens Aula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minhas Viagens Aula
+//
+//  Created by Matheus Rodrigues Araujo on 21/10/19.
+//  Copyright © 2019 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
